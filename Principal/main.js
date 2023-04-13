041251ecc0478931529a539e604a0d0481911008
Mario <marioernestomelendez@gmail.com>
2023-04-12 18:37:02 -0600

    mariusart

---
+++ Principal/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){$(".slider1").bxSlider({mode:"fade"})});document.querySelector(".menu-btn").addEventListener("click",()=>{document.querySelector(".nav-menu").classList.toggle("show")});const datos=[{img:"../Principal/imagenes/dibujos/messi.jpg",},{img:"../Principal/imagenes/dibujos/cristiano.jpg",},{img:"../Principal/imagenes/dibujos/tommy.jpg",},{img:"../Principal/imagenes/dibujos/dualipa.jpg",},{img:"../Principal/imagenes/dibujos/keanu.jpg",},{img:"../Principal/imagenes/dibujos/anuel.jpg",}];let slider1=document.querySelector(".slider1");function dinamico(src){let li=document.createElement("li");let img=document.createElement("img");img.src=src;slider1.appendChild(li);li.appendChild(img)}
+datos.forEach(({img})=>dinamico(img));const btn=document.getElementById('button');document.getElementById('form').addEventListener('submit',function(event){event.preventDefault();console.log(event);btn.value='Enviando...';const serviceID='default_service';const templateID='template_kr1cepg';emailjs.sendForm(serviceID,templateID,this).then(()=>{btn.value='Enviar';swal('Mensaje enviado!','','success')},(err)=>{btn.value='Enviar';swal(JSON.stringify(err))})})
+
+
+
+
+
+
